eee9477d594512c7ada5397c8b5d031b38ec50d6
Kimathi Sedegah <sedkim370@gmail.com>
2026-01-13 22:30:05 +0000

    Refactor page component to use ClientOnlyWrapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,20 @@
-import dynamic from "next/dynamic"
-
-const PortfolioNavbar = dynamic(() => import("@/components/PortfolioNavbar"), { ssr: false })
-const ProductTeaserCard = dynamic(() => import("@/components/ProductTeaserCard"), { ssr: false })
-const BankingScaleHero = dynamic(() => import("@/components/BankingScaleHero"), { ssr: false })
-const FAQSection = dynamic(() => import("@/components/FAQSection"), { ssr: false })
-const Footer = dynamic(() => import("@/components/Footer"), { ssr: false })
+import { PortfolioNavbar } from "@/components/PortfolioNavbar"
+import { ProductTeaserCard } from "@/components/ProductTeaserCard"
+import { BankingScaleHero } from "@/components/BankingScaleHero"
+import { FAQSection } from "@/components/FAQSection"
+import { Footer } from "@/components/Footer"
+import ClientOnlyWrapper from "@/components/ClientOnlyWrapper"
 
 export default function Page() {
   return (
     <>
       <PortfolioNavbar />
       <ProductTeaserCard />
-      <BankingScaleHero />
-      <FAQSection />
-      <Footer />
+      <ClientOnlyWrapper>
+        <BankingScaleHero />
+        <FAQSection />
+        <Footer />
+      </ClientOnlyWrapper>
     </>
   )
 }
