a425669a3e0e0589b3753c8114dda48e133aa707
Kimathi Sedegah <sedkim370@gmail.com>
2026-01-13 22:33:24 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,19 @@
+"use client"
+
 import { PortfolioNavbar } from "@/components/PortfolioNavbar"
 import { ProductTeaserCard } from "@/components/ProductTeaserCard"
 import { BankingScaleHero } from "@/components/BankingScaleHero"
 import { FAQSection } from "@/components/FAQSection"
 import { Footer } from "@/components/Footer"
-import ClientOnlyWrapper from "@/components/ClientOnlyWrapper"
 
 export default function Page() {
   return (
     <>
       <PortfolioNavbar />
       <ProductTeaserCard />
-      <ClientOnlyWrapper>
-        <BankingScaleHero />
-        <FAQSection />
-        <Footer />
-      </ClientOnlyWrapper>
+      <BankingScaleHero />
+      <FAQSection />
+      <Footer />
     </>
   )
 }
